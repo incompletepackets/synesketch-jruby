d7af3f49af32ae4262b6366d2a4856b7db73917c
Thomas Claridge <thomas.claridge@incompletepackets.co.nz>
2013-05-04 16:08:28 +1200

    added to_json method

--- lib/synesketch.rb
+++ lib/synesketch.rb
@@ -1,5 +1,6 @@
 require 'synesketch/version'
 require 'jar/synesketch.jar' 
+require 'json'
 
 module Synesketch
 
@@ -7,5 +8,20 @@ module Synesketch
 
   def self.feel text
     Empathyscope.instance.feel text
+  end
+
+  Java::SynesketchEmotion::EmotionalState.class_eval do
+    def to_json
+      { 
+        'general'   => general_weight,
+        'valence'   => valence,
+        'happiness' => happiness_weight,
+        'sadness'   => sadness_weight,
+        'anger'     => anger_weight,
+        'fear'      => fear_weight,
+        'disgust'   => disgust_weight,
+        'surprise'  => surprise_weight
+      }.to_json
+    end
   end
 end
